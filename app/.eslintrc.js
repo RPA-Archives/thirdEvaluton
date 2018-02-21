3f801b631dd0843d4ea4c971bfbe978f7417c043
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-21 10:40:33 +0530

    boilerplate

---
+++ app/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb",
+    "parser": "babel-eslint",
+    "rules": {
+    'react/jsx-filename-extension': 0,
+    'react/prefer-stateless-function': 0
+    }
+};
